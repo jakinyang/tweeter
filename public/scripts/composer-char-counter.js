80ad54341ee71aebb50dd08d9b071635c474027b
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-07 21:48:56 -0800

    Add scripts to handle events
    Same as previous commit: adds events listeners to handle input event on text area and to adjust output accordinly.
    
    Submit event default behaviour is removed and instead chains a reset of the textarea, the output, and refocuses on the textarea.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  const $tweetForm = $('form');
+  const $tweetTextArea = $('textarea');
+  let $tweetOutput = $('output');
+
+  // Event listener for text input field
+  $tweetTextArea.on('input', function() {
+    const $this = $(this);
+    $tweetOutput.text(140 - this.value.length);
+    if($tweetOutput.text() < 0) {
+      $this.next().children('output').css('color', 'red');
+    } else {
+      $this.next().children('output').css('color', 'black');
+    }
+  })
+
+  // Event listener for submit button
+  $tweetForm.on('submit', function(event) {
+    event.preventDefault();
+    $tweetTextArea.val('');
+    $tweetTextArea.focus();
+    $tweetOutput.text(140);
+  })
+});

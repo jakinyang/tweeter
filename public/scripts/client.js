73c55eacbb96ef63358b20dd1ff0beed1cf3a7f7
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-08 14:45:11 -0800

    Add createTweetElement and renderTweets
    createTweetElement takes in an object with tweet data values and renders an html template populated with the values from the tweet object argument.
    
    renderTweets takes an array/object of tweet objects and cycles through them calling createTweetElement on each tweet object. It then appends the generated tweet HTML to the tweet-log section.
    
    If more modularity is needed, these functions could be generalised to take more arguments and be flexible in what the target is to append to.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,18 +4,33 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+console.log('Client-side.js loaded');
 // Test Driver code
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 /**
  * takes a tweet string as an argument
@@ -25,15 +40,15 @@ const createTweetElement = function(tweet) {
   const $tweet = $(`
     <article class="tweet">
       <header>
-        <div class="tweet-user">
-          <i class="fa-regular fa-face-meh-blank"></i>
-          <p class="username">John Cena</p>
+        <div class="user">
+          <img src="${tweet.user.avatars}" />
+          <p class="user-name">${tweet.user.name}</p>
         </div>
-        <p class="user-id">@cantseeyou</p>
+        <p class="user-handle">${tweet.user.handle}</p>
       </header>
-      <p class="tweet-content">Whatever cold-pressed cardigan, gatekeep kogi street art thundercats intelligentsia.</p>
+      <p class="tweet-content">${tweet.content.text}</p>
       <footer>
-        <p><span class="tweet-age">10</span> days ago</p>
+        <p><span class="tweet-age">${tweet.created_at}</span> days ago</p>
         <div class="tweet-button-group">
           <i class="fa-solid fa-flag fa-2xs"></i>
           <i class="fa-solid fa-retweet fa-2xs"></i>
@@ -41,5 +56,14 @@ const createTweetElement = function(tweet) {
         </div>
       </footer>
     </article>
-  `)
+  `);
+  return $tweet;
 }
+
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    $('.tweet-log').append(createTweetElement(tweet));
+  }
+}
+
+renderTweets(data);
